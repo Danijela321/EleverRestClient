ceff0990614b98f424bd46a2069bb8855c33aeed
Danijela <56878144+Danijela321@users.noreply.github.com>
2020-05-16 21:40:58 +0200

    json/xml

--- src/TestClient.java
+++ src/TestClient.java
@@ -19,18 +19,18 @@ public class TestClient {
 	public static void main(String[] args) {
 
 		/**
-		 * @author danijela
-		 * hela klass
+		 * @author danijela hela klass
 		 */
-		
+
 		Client client = ClientBuilder.newClient();
 		// obs.firts letar efter med efternamn och sen med id
 		// man letar efter elever som heter Lundqvist och har id=4
 		Response response = client.target("http://localhost:8080/EleverManagement/webservice/elever/Lundqvist/4")
-				.request().buildGet().invoke();
+				.request("application/JSON").buildGet().invoke();
 
-		Elever elever = response.readEntity(Elever.class);
-		System.out.println(elever);
+		// Elever elever = response.readEntity(Elever.class);
+		// System.out.println(elever);
+		System.out.println(response.readEntity(String.class));
 
 		response.close();
 
@@ -40,18 +40,17 @@ public class TestClient {
 		nyElever.setSkola("Humanus");
 		nyElever.setKlass(1);
 
-		Entity nyEleverEntity = Entity.entity(nyElever, "application/XML");
-
-		response = client.target("http://localhost:8080/EleverManagement/webservice/elever").request()
-				.buildPost(nyEleverEntity).invoke();
+		 //Entity nyEleverEntity = Entity.entity(nyElever, "application/XML");
+		Entity nyEleverEntity = Entity.entity(nyElever, "application/JSON");
+		response = client.target("http://localhost:8080/EleverManagement/webservice/elever").request().buildPost(nyEleverEntity).invoke();
 		System.out.println();
-		System.out.println(response.readEntity(Elever.class).getId());
+		 System.out.println(response.readEntity(Elever.class).getId());
+		//System.out.println(response.readEntity(String.class));
 
 		response.close();
 
 		// får alla elever
-		response = client.target("http://localhost:8080/EleverManagement/webservice/elever").request().buildGet()
-				.invoke();
+		response = client.target("http://localhost:8080/EleverManagement/webservice/elever").request("application/XML").buildGet().invoke();
 		System.out.println();
 		// System.out.println(response.readEntity(String.class));
 		List<Elever> eleverAlla = response.readEntity(new GenericType<List<Elever>>() {
@@ -60,7 +59,7 @@ public class TestClient {
 		for (Elever el : eleverAlla) {
 			System.out.println(el);
 		}
-		response.close(); 
+		response.close();
 	}
 
 }
